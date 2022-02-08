d5d792be9081d1aefbc4ec045f6cce81d64be1ab
Frappuccino001 <fumi00hiro@icloud.com>
2022-02-08 23:53:21 +0900

    FizzBuzz

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fizz/calculate"
+	"fmt"
+)
+
+func main() {
+	for i := 1; i < 31; i++ {
+		if calculate.Fizz(i) && calculate.Buzz(i) {
+			fmt.Println("FizzBuzz!")
+		} else if calculate.Fizz(i) {
+			fmt.Println("Fizz")
+		} else if calculate.Buzz(i) {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+}
